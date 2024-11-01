d5593e11fdaefae6ee04721486536f9027bf016e
Adria Blancafort <adriablancafort@gmail.com>
2024-11-01 23:52:02 +0100

    hardcode db string

--- medusa-config.ts
+++ medusa-config.ts
@@ -4,7 +4,7 @@ loadEnv(process.env.NODE_ENV || 'development', process.cwd())
 
 module.exports = defineConfig({
   projectConfig: {
-    databaseUrl: process.env.DATABASE_URL,
+    databaseUrl: "postgres://postgres:dd8d48a262b6e9cc@srv-captain--medusa-server:5432/postgres",
     databaseDriverOptions: {
       connection: {
         ssl: {
